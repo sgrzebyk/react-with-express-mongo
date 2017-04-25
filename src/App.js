c374ead378245d0c1ff361a285d90bcacb691c0d
SGrzebyk <sgrzebyk@sgrzebyk.pl>
2017-04-25 16:14:14 +0200

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import {Main} from './components/main'
+
+
+class App extends Component {
+  render() {
+    return (
+            <Main/>
+          );
+  }
+}
+
+export default App;
---
+++ src/components/main.js
@@ -0,0 +1,22 @@
+/**
+ * Created by oem on 2017-04-25.
+ */
+import React, {Component}from 'react';
+
+
+export class Main extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    render() {
+        const result = (
+            <div>
+               Main Component
+            </div>
+        )
+        return result;
+    }
+}
